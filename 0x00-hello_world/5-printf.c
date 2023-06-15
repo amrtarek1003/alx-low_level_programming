33acf7312630aa44b0a9d490d9fc3e5d574cd19f
amretarek1003 <amrtarek90.at@gmail.com>
2023-06-15 15:36:08 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+
+/**
+ * this function is for ALX SE Program
+ * required is to print line below
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
