92fb5401aa5d9cec34ae85d2b61f9ea954609efe
amretarek1003 <amrtarek90.at@gmail.com>
2023-06-15 16:06:23 -0700

    please end it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints a string to the standard error.
- *
- * Return: Always 1 (Error)
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 1 (Success)
  */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    size_t message_len = strlen(message);
-
-    // Write the message to the standard error using the write system call
-    write(STDERR_FILENO, message, message_len);
-
-    // Return 1 to indicate an error occurred
-    return 1;
+		write(2,
+				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
