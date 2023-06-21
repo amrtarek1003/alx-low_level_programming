4aaca015a2748531f44afb3fce111f3b4c73ac05
amretarek1003 <amrtarek90.at@gmail.com>
2023-06-21 01:17:46 -0700

    fix indent again

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,9 +13,12 @@ void jack_bauer(void)
 	int min2 = 0;
 	int min1 = 0;
 
-	while (hour2 <= 2) {
-		while (hour1 <= 9) {
-			while (min2 <= 5) {
+	while (hour2 <= 2)
+	{
+		while (hour1 <= 9)
+		{
+			while (min2 <= 5)
+			{
 				while (min1 <= 9)
 				{
 					printf("%i%i:%i%i\n", hour2, hour1, min2, min1);
@@ -26,9 +29,8 @@ void jack_bauer(void)
 			}
 			hour1++;
 			min2 = 0;
-			if (hour2 == 2 && hour1 == 4) {
+			if (hour2 == 2 && hour1 == 4) 
 				break;
-			}
 		}
 		hour2++;
 		hour1 = 0;
