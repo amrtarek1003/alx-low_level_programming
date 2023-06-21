e0bc8e4d226f79a5918400e2d691c1cb0d91540b
amretarek1003 <amrtarek90.at@gmail.com>
2023-06-21 01:14:41 -0700

    Should fix indent

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,28 +8,29 @@
  */
 void jack_bauer(void)
 {
-    int hour2 = 0;
-    int hour1 = 0;
-    int min2 = 0;
-    int min1 = 0;
+	int hour2 = 0;
+	int hour1 = 0;
+	int min2 = 0;
+	int min1 = 0;
 
-    while (hour2 <= 2) {
-        while (hour1 <= 9) {
-            while (min2 <= 5) {
-                while (min1 <= 9) {
-                    printf("%i%i:%i%i\n", hour2, hour1, min2, min1);
-                    min1++;
-                }
-                min2++;
-                min1 = 0;
-            }
-            hour1++;
-            min2 = 0;
-            if (hour2 == 2 && hour1 == 4) {
-                break;
-            }
-        }
-        hour2++;
-        hour1 = 0;
-    }
+	while (hour2 <= 2) {
+		while (hour1 <= 9) {
+			while (min2 <= 5) {
+				while (min1 <= 9)
+				{
+					printf("%i%i:%i%i\n", hour2, hour1, min2, min1);
+					min1++;
+				}
+				min2++;
+				min1 = 0;
+			}
+			hour1++;
+			min2 = 0;
+			if (hour2 == 2 && hour1 == 4) {
+				break;
+			}
+		}
+		hour2++;
+		hour1 = 0;
+	}
 }
