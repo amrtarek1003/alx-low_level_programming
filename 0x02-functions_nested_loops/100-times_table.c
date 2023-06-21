4965af4d0cbd92039dc7e4c364adb12a91ef0f42
amretarek1003 <amrtarek90.at@gmail.com>
2023-06-21 14:24:14 -0700

    tired

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_time_table - print times table for enterend number from user
+ * number shouldn't be less than 0 or greater than 15
+ *
+ * Return: (1) if entered wrong input or (0) if success)
+ */
+
+void print_times_table(int n)
+{
+        int x =0;
+        int y =0;
+
+        if(n>15 || n<0)
+        {
+                return;
+        }
+        while(x <= n)
+        {
+                while(y <=n)
+                {
+                                if(x*y<10)
+                                {
+                                        _putchar(x*y + '0');
+					if(y != n)
+                                        {
+                                                _putchar(',');
+                                        }
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                }
+                                if(x*y >= 10 && x*y<100)
+                                {
+                                        _putchar(x*y/10 + '0');
+					_putchar((x*y)%10 + '0');
+                                        if(y != n)
+                                        {
+                                                _putchar(',');
+                                        }
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                }
+                                if(x*y >= 100)
+                                {
+                                        _putchar(x*y/100 + '0');
+					_putchar(((x*y)%100)/10 +'0');
+					_putchar((x*y)%10 + '0');
+
+
+					if(y != n)
+                                        {
+						_putchar(',');
+					}
+					_putchar(' ');
+				}
+				y++;
+		}
+		x++;
+		y = 0;
+		_putchar('\n');
+	}
+}
