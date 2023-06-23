6b4f19327f29050da18066e7427eca0f9488bf91
amretarek1003 <amrtarek90.at@gmail.com>
2023-06-23 00:28:28 -0700

    Fix error

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,28 +10,25 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (a > c && c > b)
-    {
-	largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else if (b > c && c > a)
-    {
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
 	largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	}
+	else
+	{
+	largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }
+
