1e7c2b6d43bc3eda5462d3861d81902bb29af180
amretarek1003 <amrtarek90.at@gmail.com>
2023-06-26 05:44:09 -0700

    0x05 Task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - This function swap the variables that two pointers point to
+ *
+ * @a: pointer a that point to variable a
+ * 
+ * @b: pointer a that point to variable b
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
