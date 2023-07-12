5b1295955161ef389e63719ccb035fd402a01bee
amretarek1003 <amrtarek90.at@gmail.com>
2023-07-11 22:03:20 -0700

    FINAAAAAALY

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - function that return the length of a string
+ *
+ * @s: the string that the function return the legth of
+ *
+ * This function takes a string as a parameter and return its length
+ *
+ * Return: Length of the string excluding '\0' charachter
+ */
+
+int _strlen(char *s)
+{
+	int tmp = 0;
+	char *a = s;
+
+	while (*a != '\0')
+	{
+		tmp++;
+		a++;
+	}
+	return (tmp);
+}
