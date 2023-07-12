38a1f56a9ffc690f9ef2da571ce8d79f3f2c03d6
amretarek1003 <amrtarek90.at@gmail.com>
2023-07-11 20:06:25 -0700

    Task 6 Recursion Again

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,29 +4,33 @@
  * is_prime_number - Function that returns 1 if the input integer is a prime number -
  * - other wise the function return 0
  *
- *   Return : int number 0 (Fail) or 1 (Success)
+ *   @n: the number to check
+ *
+ *   Return: int number 0 (Fail) or 1 (Success)
  */
 
+int is_prime_helper(int n, int i);
 
 int is_prime_number(int n)
 {
-	int i = 2;
+        if (n <= 1) {
+                return 0; /* Not a prime number */
+        }
 
-	if (n <= 1)
-	{
-		return 0;
-	}
+        /* Start recursion with i = 2 */
+        return is_prime_helper(n, 2);
+}
 
-	if (i * i > n)
-	{
-		return 1;
-	}
+int is_prime_helper(int n, int i)
+{
+        if (i * i > n) {
+                return 1; /* Is a prime number */
+        }
 
-	if (n % i == 0)
-	{
-		return 1;
-	}
+        if (n % i == 0) {
+                return 0; /* Not a prime number */
+        }
 
-	i++;
-	return (is_prime_number(int n);
+        /* Recurse with i + 1 */
+        return is_prime_helper(n, i + 1);
 }
